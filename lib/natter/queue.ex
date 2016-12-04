b1dc94ec74bf17bde6164a8c5c7522e92e06d80a
Louis Pilfold <louis@lpil.uk>
2016-12-04 16:52:49 +0000

    :queue wrapper

---
+++ lib/natter/queue.ex
@@ -0,0 +1,70 @@
+defmodule Natter.Queue do
+  @moduledoc false
+
+  defstruct q: :queue.new()
+  @type t :: :queue.queue
+
+  @doc """
+  Construct a new queue from the given list.
+  """
+  @spec new([any]) :: t
+  def new(xs \\ []) when is_list(xs) do
+    %__MODULE__{q: :queue.from_list(xs)}
+  end
+
+
+  @doc """
+  Enqueue an item.
+
+      iex> q = new() |> enqueue(:hello)
+      iex> dequeue(q)
+      {:value, :hello, new()}
+  """
+  @spec enqueue(t, any) :: t
+  def enqueue(%{q: q}, x) do
+    %__MODULE__{q: :queue.in(x, q)}
+  end
+
+
+  @doc """
+  Dequeue the next item from the queue.
+
+      iex> q = new() |> enqueue(:hello)
+      iex> dequeue(q)
+      {:value, :hello, new()}
+
+      iex> q = new()
+      iex> dequeue(q)
+      :empty
+  """
+  @spec dequeue(t) :: {:value, any, t} | :empty
+  def dequeue(%{q: q}) do
+    case :queue.out(q) do
+      {{:value, value}, q} -> {:value, value, %__MODULE__{q: q}}
+      _ -> :empty
+    end
+  end
+
+
+  @doc """
+  Peek at the next item without removing it.
+
+      iex> q = new() |> enqueue(:hello)
+      iex> peek(q)
+      {:value, :hello}
+
+      iex> q = new()
+      iex> peek(q)
+      :empty
+  """
+  @spec peek(t) :: {:value, any} | :empty
+  def peek(%{q: q}) do
+    :queue.peek(q)
+  end
+end
+
+defimpl Inspect, for: Natter.Queue do
+  def inspect(%{q: q}, _) do
+    "#Natter.Queue<#{inspect :queue.to_list(q)}>"
+  end
+end
